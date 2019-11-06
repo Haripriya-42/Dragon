61b9aa237419b4cda2aa34a68ad5cf11533cf9ca
unknown <hariipriyah@gmail.com>
2019-11-06 10:29:28 +0530

    java added

---
+++ javaFundamentals2/src/javaFundamentals2/HelloWorld.java
@@ -0,0 +1,11 @@
+package javaFundamentals2;
+
+public class HelloWorld {
+
+		public static void main(String[] args)
+	{
+			for(int i=0; i <4; i++)
+					System.out.println("Hello Java..." +i);
+            //return ;
+	}
+}
